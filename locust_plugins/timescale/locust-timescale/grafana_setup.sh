44c03456ca2e533898196efbcfdeb6df6b875c28
Collins <delanhype@gmail.com>
2022-02-20 13:11:20 +0300

    Fix operational issues and create a workflow to build-push the image

--- locust_plugins/timescale/locust-timescale/grafana_setup.sh
+++ locust_plugins/timescale/locust-timescale/grafana_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
 # edit this as necessary, if you want to set up the DS/dashboards on a different environment
-export GRAFANA_HOST="${GRAFANA_HOST:=http://grafana:3000}"
-export GRAFANA_CRED="${GRAFANA_CRED:=admin:admin}"
+export GRAFANA_HOST="${GRAFANA_HOST}"
+export GRAFANA_CRED="${GRAFANA_CRED}"
 export GRAFANA_OVERWRITE="${GRAFANA_OVERWRITE:=false}" # change to true to overwrite pre-existing dashboards (update to latest version from grafana.com)
 export PGHOST="${PGHOST:=postgres}"
 export PGPORT="${PGPORT:=5432}"
+export PGPASSWORD=${POSTGRES_PASSWORD}
 export DS_NAME="${DS_NAME:=locust_timescale}"
 
 ${BASH_SOURCE%/*}/create_datasource.sh
