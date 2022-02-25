9e49a2acf28b1b3cfaf27ea6ed944d6bc143d31a
Collins <delanhype@gmail.com>
2022-02-25 08:48:51 +0300

    Fix dashboard import script to use API token

--- locust_plugins/timescale/locust-timescale/import_dashboards.sh
+++ locust_plugins/timescale/locust-timescale/import_dashboards.sh
@@ -2,8 +2,8 @@
 ds=(10878 14423 14422 15419);
 for d in "${ds[@]}"; do
   echo -n "Processing $d: "
-  j=$(curl -s -k -u "$GRAFANA_CRED" $GRAFANA_HOST/api/gnet/dashboards/$d | jq .json)
-  curl -s -k -u "$GRAFANA_CRED" -XPOST -H "Accept: application/json"\
+  j=$(curl -H "Authorization: Bearer $GRAFANA_CRED" $GRAFANA_HOST/api/gnet/dashboards/$d | jq .json)
+  curl -H "Authorization: Bearer $GRAFANA_CRED" -XPOST -H "Accept: application/json"\
     -H "Content-Type: application/json"\
     -d "{\"dashboard\":$j,\"overwrite\":$GRAFANA_OVERWRITE,\"inputs\":[{\"name\":\"DS_LOCUST\",\"type\":\"datasource\", \"pluginId\":\"postgres\",\"value\":\"$DS_NAME\"}]}"\
     $GRAFANA_HOST/api/dashboards/import; echo ""
